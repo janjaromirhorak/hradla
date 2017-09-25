71bececdb8675dba0576638f5f505b6e570cfcc1
Jan Jaromír Horák <janjaromirhorak@gmail.com>
2017-09-25 16:03:22 +0200

    Revert "improved comments"
    This reverts commit b4c8be164988493792dd823d07257cb7df9ddb65.

--- es6/contextMenu.js
+++ es6/contextMenu.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// item of the context menu
 class ContextMenuItem {
     constructor(name, type, contextMenu, parentSVG, clickFunction) {
         this.name = name;
@@ -8,32 +7,26 @@ class ContextMenuItem {
         this.contextMenu = contextMenu;
         this.parentSVG = parentSVG;
 
-        // create the jQuery element
         this.$el = $("<li>");
         $(this.$el)
             .text(name)
             .attr("type", type);
 
-        // assign a click functionality to the element
         if(clickFunction) {
             $(this.$el).click(
                 (event) => {
-                    // call the passed function
                     clickFunction(event);
-                    // hide the menu
                     contextMenu.hide();
                 }
             );
         }
     }
 
-    // add a css class to the element
     addClass(cls) {
         this.$el.addClass(cls);
         return this;
     }
 
-    // make this item a sub-menu (append child items)
     appendItem(item) {
         if(!this.subList) {
             this.subList = $("<ul>");
@@ -45,13 +38,10 @@ class ContextMenuItem {
         return item;
     }
 
-    // get the jQuery representation of this menu item
     get jQuery() {
         return this.$el;
     }
 }
-
-// specific functionality for the context menu items that allow to add gates
 class GateMenuItem extends ContextMenuItem {
     constructor(type, contextMenu, parentSVG) {
         super(
@@ -75,35 +65,27 @@ class GateMenuItem extends ContextMenuItem {
     }
 }
 
-// the main class specifying the context menu
 export default class ContextMenu {
     constructor(parentSVG) {
         this.parentSVG = parentSVG;
 
-        // list of gates that can be added
         const gates = ["not", "and", "or", "nand", "nor", "xor", "xnor"];
 
-        // starting position of the context menu (left top corner)
         this.position = {
             x: 0, y: 0
         };
 
-        // the main jQuery element
         this.$el = $("<ul>");
-        // add id contextMenu (used in SCSS for styling)
         this.$el.attr('id', 'contextMenu');
 
-        // create a submenu with gates
         let gateList = new ContextMenuItem("New gate", '', this, parentSVG);
         for (let i = 0 ; i < gates.length ; ++i) {
             gateList.appendItem(
                 new GateMenuItem(gates[i], this, parentSVG)
             );
         }
-        // append the submenu with gates
         this.appendItem(gateList);
 
-        // create the button for adding an input box
         this.appendItem(
             new ContextMenuItem("Input box", '', this, parentSVG,
                 () => {
@@ -117,7 +99,6 @@ export default class ContextMenu {
             )
         );
 
-        // create the button for adding an output box
         this.appendItem(new ContextMenuItem("Output box", '', this, parentSVG, () => {
             let position = {
                 left: this.parentSVG.snapToGrid(this.position.x),
@@ -127,15 +108,12 @@ export default class ContextMenu {
             parentSVG.newOutput(position.left, position.top);
         }));
 
-        // conditional items for removing boxes and wires
         this.appendConditionalItem('box', 'Remove this item', (id) => {this.parentSVG.removeBox(id)});
         this.appendConditionalItem('wire', 'Remove this wire', (id) => {this.parentSVG.removeWireById(id)});
 
-        // add the jQuery representation before the svg jQuery element
         parentSVG.$svg.before(this.$el);
     }
 
-    // append an item to the menu
     appendItem(item) {
         this.$el.append(item.jQuery);
         return item;
